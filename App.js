1808a068d71f193da59318d486d6e5ad9ae96c00
Amar Moorer <amarmoorer@macbook-pro.myfiosgateway.com>
2022-04-11 15:44:54 -0400

    lab

---
+++ App.js
@@ -0,0 +1,9 @@
+const express = require('express');
+//create an instance of express, development standards normally use app as the instance name
+const app = express();
+//use the get method to set what happens when we hit a certain route or url endpoint
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+//set the port that the app should listen to
+app.listen(4000);
